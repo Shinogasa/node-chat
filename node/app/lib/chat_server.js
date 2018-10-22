7f29136e225bfcb24c4d34eaa66ab5252f13d27d
Shinogasa <cuulbulu@gmail.com>
2018-10-23 00:07:49 +0900

    ゲスト名の割当，ルームへの参加処理の実装

--- node/app/lib/chat_server.js
+++ node/app/lib/chat_server.js
@@ -30,3 +30,48 @@ exports.listen = (server) => {
         handleClientDisconnection(socket, nickNames, nameUsed);
     });
 };
+
+function assignGuestName(socket, guestNumber, nickNames, nameUsed) {
+    let name = 'Guest' + guestNumber;
+
+    nickName[socket.id] = name;
+
+    socket.emit('nameResult', {
+        success: true,
+        name: name
+    });
+    nameUsed.push(name);
+
+    return guestNumber + 1;
+
+}
+
+
+function joinRoom(socket, room) {
+    socket.join(room);
+
+    currentRoom[socket.id] = room;
+
+    socket.emit('joinResult', {room: room});
+
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + ' has joinbed ' + room + '.'
+    });
+
+    const usersInRoom = io.sockets.clients(room);
+
+    if ( usersInRoom.length > 1 ) {
+        const usersInRoomSummary = 'Users currently in ' + room + ': ';
+        for ( const index in usersInRoom ) {
+            const userSocketId = usersInRoom[index].id;
+            if  ( userSocketId != socket.id ) {
+                if ( index > 0 ) {
+                    usersInRoomSummary += ', ';
+                }
+                usersInRoomSummary += nickNames[userSoxketId];
+            }
+        }
+        usersInRoomSummary += '.';
+        socket.emit('message', {text: usersInRoomSummary});
+    }
+ }
